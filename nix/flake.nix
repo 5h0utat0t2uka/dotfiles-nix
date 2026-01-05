23606c363cdb78b1e85e19811b52ff2274f257ef
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-01-05 21:35:04 +0900

    update

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,60 +3,67 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    { self, nixpkgs, home-manager, darwin, ... } @inputs:
+  outputs = { self, nixpkgs, home-manager, darwin, ... } @inputs:
     let
-      hostKey = "A3112";
-      identity = import ./hosts/darwin/${hostKey}/identity.nix;
+      lib = nixpkgs.lib;
 
-      system = identity.system;
-      username = identity.username;
-      hostname = identity.hostname;
+      # ./hosts/darwin 配下のディレクトリ名（= hostKey）を列挙
+      darwinHostsDir = ./hosts/darwin;
+      darwinHostKeys = lib.attrNames(lib.filterAttrs (_name: type: type == "directory")(builtins.readDir darwinHostsDir));
+
+      identityFor = hostKey: import (darwinHostsDir + "/${hostKey}/identity.nix");
+      mkDarwinConfig = hostKey:
+        let
+          identity = identityFor hostKey;
+        in
+        # 方式Aの前提：ディレクトリ名と identity.hostname を一致させる
+        assert (identity ? hostname);
+        assert (identity.hostname == hostKey);
+        {
+          name = identity.hostname;
+          value = darwin.lib.darwinSystem {
+            system = identity.system;
+            specialArgs = { inherit identity inputs; };
+            modules = [
+              # nixpkgs 設定
+              {
+                nixpkgs = {
+                  system = identity.system;
+                  config.allowUnfree = true;
+                };
+              }
+
+              # home-manager を nix-darwin 経由で統合
+              home-manager.darwinModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+
+                # 既存 home-manager.nix が identity を要求する前提
+                home-manager.extraSpecialArgs = { inherit identity; };
+                home-manager.users.${identity.username} = import ./modules/darwin/home-manager.nix;
+              }
+              # ホスト入口（imports で system/homebrew を読む）
+              (darwinHostsDir + "/${hostKey}/default.nix")
+            ];
+          };
+        };
+
+      darwinConfigurations = builtins.listToAttrs (map mkDarwinConfig darwinHostKeys);
     in
     {
-      darwinConfigurations.${hostname} = darwin.lib.darwinSystem {
-        inherit system;
-
-        # modules 側でも identity を参照できるようにする
-        specialArgs = { inherit identity inputs; };
-
-        modules = [
-          # nixpkgs 設定（allowUnfree など）
-          {
-            nixpkgs = {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          }
-
-          # home-manager を nix-darwin 経由で統合
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            # home-manager 側にも identity を渡す（あなたの既存 home-manager.nix が identity を要求）
-            home-manager.extraSpecialArgs = { inherit identity; };
-
-            # HM user 定義（home-manager.nix 側で home.username 等を設定）
-            home-manager.users.${username} = import ./modules/darwin/home-manager.nix;
-          }
-
-          # ホスト入口（imports で system/homebrew を読む）
-          ./hosts/darwin/${hostKey}/default.nix
-        ];
-      };
+      inherit darwinConfigurations;
+      # 任意：フォーマッタ（ある環境だけで有効化したいなら消してOK）
+      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
     };
 }
