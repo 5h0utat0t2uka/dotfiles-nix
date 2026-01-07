237d75555cc2ee12adee8fa7eb077851dcb6db36
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-01-07 17:41:49 +0900

    update

--- nix/modules/darwin/system.nix
+++ nix/modules/darwin/system.nix
@@ -33,6 +33,33 @@ in
           askForPassword = 1;
           askForPasswordDelay = 0;
         };
+        "com.apple.Spotlight" = {
+          orderedItems = [
+            { enabled = 1; name = "APPLICATIONS"; }
+            { enabled = 1; name = "SHORTCUTS"; }
+            # 以下、不要な項目をすべて無効化
+            { enabled = 0; name = "ACTIONS"; }
+            { enabled = 0; name = "CLIPBOARD"; }
+            { enabled = 0; name = "DIRECTORIES"; }
+            { enabled = 0; name = "DOCUMENTS"; }
+            { enabled = 0; name = "PDF"; }
+            { enabled = 0; name = "MESSAGES"; }
+            { enabled = 0; name = "CONTACT"; }
+            { enabled = 0; name = "EVENT_TODO"; }
+            { enabled = 0; name = "IMAGES"; }
+            { enabled = 0; name = "BOOKMARKS"; }
+            { enabled = 0; name = "MUSIC"; }
+            { enabled = 0; name = "MOVIES"; }
+            { enabled = 0; name = "PRESENTATIONS"; }
+            { enabled = 0; name = "SPREADSHEETS"; }
+            { enabled = 0; name = "SOURCE"; }
+            { enabled = 0; name = "MENU_DEFINITION"; }
+            { enabled = 0; name = "MENU_OTHER"; }
+            { enabled = 0; name = "MENU_CONVERSION"; }
+            { enabled = 0; name = "MENU_EXPRESSION"; }
+            { enabled = 0; name = "MENU_SPOTLIGHT_SUGGESTIONS"; }
+          ];
+        };
         # "com.apple.screencapture" = {
         #   location = "~/Documents";
         #   type = "png";
