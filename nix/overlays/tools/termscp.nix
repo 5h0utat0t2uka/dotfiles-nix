6345d17f2fcb88cbe331eb6fa60074c2ce349b85
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-01-19 00:58:12 +0900

    update

--- nix/overlays/tools/termscp.nix
+++ nix/overlays/tools/termscp.nix
@@ -1,6 +1,10 @@
 final: prev: {
   termscp = prev.termscp.overrideAttrs (old: {
     doCheck = false;
+    doInstallCheck = false;
     checkPhase = "true";
+    installCheckPhase = "true";
+
+    nativeInstallCheckInputs = [];
   });
 }
