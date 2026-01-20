114e85b58314e5601f07a4831f29768467c5660b
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-01-20 16:42:56 +0900

    Update .config/zsh/.p10k.zsh

--- dot_config/zsh/dot_p10k.zsh
+++ dot_config/zsh/dot_p10k.zsh
@@ -48,7 +48,6 @@
   # last prompt line gets hidden if it would overlap with left prompt.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    newline
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
