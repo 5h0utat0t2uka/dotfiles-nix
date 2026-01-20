fbb020410ad6773b4c9bd031c075a4b14cfbc25a
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-01-20 19:26:34 +0900

    Update .config/zsh/.p10k.zsh

--- dot_config/zsh/dot_p10k.zsh
+++ dot_config/zsh/dot_p10k.zsh
@@ -1662,8 +1662,8 @@
 
   ####################################[ time: current time ]####################################
   # Current time color.
-  typeset -g POWERLEVEL9K_TIME_FOREGROUND=4
-  # typeset -g POWERLEVEL9K_TIME_FOREGROUND=6
+  # typeset -g POWERLEVEL9K_TIME_FOREGROUND=4
+  typeset -g POWERLEVEL9K_TIME_FOREGROUND=6
   # Format for the current time: 09:51:02. See `man 3 strftime`.
   typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
   # If set to true, time will update when you hit enter. This way prompts for the past
