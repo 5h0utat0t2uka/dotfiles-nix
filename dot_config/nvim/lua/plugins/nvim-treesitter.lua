30c1fbca4dccdc3fd9b11661c364bec94cbf91cf
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-02-15 21:00:30 +0900

    Update .config/nvim/lua/plugins/nvim-treesitter.lua

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,  -- lazy-loadingはサポートされていない
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter").setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
-        "lua", "vim", "vimdoc", "bash",
-        "typescript", "tsx", "json", "html", "javascript"
-      },
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+    require("nvim-treesitter").install({
+      "lua", "vim", "vimdoc", "bash",
+      "typescript", "tsx", "json", "html", "javascript"
     })
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "json", "typescript", "javascript", "tsx", "html" },
