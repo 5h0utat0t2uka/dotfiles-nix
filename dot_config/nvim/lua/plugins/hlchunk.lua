30b8cb7d6dd630a972b727d5e85eca8882b0a0d8
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-02-15 20:12:24 +0900

    Update .config/nvim/lua/plugins/hlchunk.lua

--- dot_config/nvim/lua/plugins/hlchunk.lua
+++ dot_config/nvim/lua/plugins/hlchunk.lua
@@ -1,16 +1,10 @@
 return {
   "shellRaining/hlchunk.nvim",
-  -- event = { "BufReadPre", "BufNewFile" },
-  -- event = "VeryLazy",
-  lazy = false,
-  config = function()
-    require("hlchunk").setup({
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
       chunk = {
         enable = true,
-        style = {
-          { fg = "#81A1C1" },
-          { fg = "#D08770" },
-        },
+        use_treesitter = true,
         chars = {
           horizontal_line = "─",
           vertical_line = "│",
@@ -18,12 +12,38 @@ return {
           left_bottom = "└",
           right_arrow = "─",
         },
+        style = {
+          { fg = "#81A1C1" },
+          { fg = "#bf616a" },
+        },
       },
       indent = {
         enable = true,
         chars = { "│" },
         style = { { fg = "#434C5E" } },
       },
-    })
-  end
+    },
+  -- config = function()
+  --   require("hlchunk").setup({
+  --     chunk = {
+  --       enable = true,
+  --       style = {
+  --         { fg = "#81A1C1" },
+  --         { fg = "#D08770" },
+  --       },
+  --       chars = {
+  --         horizontal_line = "─",
+  --         vertical_line = "│",
+  --         left_top = "┌",
+  --         left_bottom = "└",
+  --         right_arrow = "─",
+  --       },
+  --     },
+  --     indent = {
+  --       enable = true,
+  --       chars = { "│" },
+  --       style = { { fg = "#434C5E" } },
+  --     },
+  --   })
+  -- end
 }
