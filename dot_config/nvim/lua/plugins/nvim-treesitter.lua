f4d84bb453512ac114baa3cac984577093ab3b67
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-02-15 20:54:28 +0900

    Update .config/nvim/lua/plugins/nvim-treesitter.lua

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,21 +7,8 @@ return {
       indent = { enable = true },
       ensure_installed = {
         "lua", "vim", "vimdoc", "bash",
-        "tsx", "json", "html", "typescript", "javascript"
+        "typescript", "tsx", "json", "html", "javascript"
       },
-      auto_install = true,  -- ← 自動インストールを有効化
     })
-    -- パーサーが存在する場合のみ有効化
-    -- vim.api.nvim_create_autocmd("FileType", {
-    --   pattern = "*",
-    --   callback = function(args)
-    --     local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
-    --     if not lang then return end
-    --     local ok = pcall(vim.treesitter.start, args.buf, lang)
-    --     if not ok then
-    --       return
-    --     end
-    --   end,
-    -- })
   end,
 }
