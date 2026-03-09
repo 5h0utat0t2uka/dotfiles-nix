7b095f792eb720c5638146fd907b314f6af1c830
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-10 08:14:03 +0900

    Add .config/nvim/lua/plugins/lspconfig.lua

---
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,29 @@
+-- lua/plugins/lspconfig.lua
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("lua_ls")
+
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("ts_ls")
+
+      vim.lsp.config("astro", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("astro")
+
+      vim.lsp.config("eslint", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("eslint")
+    end,
+  },
+}
