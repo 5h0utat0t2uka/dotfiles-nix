3b9acd16f9e66000eee0d9df39ac705590caea48
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-10 05:30:10 +0900

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,12 +1,10 @@
 -- ~/.config/nvim/lua/config/autocmds.lua
 
-local english_im = "com.apple.keylayout.ABC"
+local english_im = "com.apple.inputmethod.Kotoeri.RomajiTyping.Roman"
 
 vim.api.nvim_create_autocmd("InsertLeave", {
   group = vim.api.nvim_create_augroup("AutoSwitchIM", { clear = true }),
   callback = function()
-    if vim.fn.executable("macism") == 1 then
-      vim.fn.system({ "macism", english_im })
-    end
+    vim.fn.system({ "macism", english_im })
   end,
 })
