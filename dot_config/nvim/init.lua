0cf562b79301df547ec98eaca14ad45a546861e4
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-10 05:12:31 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,11 +1,13 @@
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- require configs
 require("config.options")
 require("config.keymaps")
+require("config.autocmds")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
