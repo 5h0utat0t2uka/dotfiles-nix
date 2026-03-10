36badd03b83e523274175d2dd5b0c736d7c03eb0
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-11 00:11:58 +0900

    Update .config/nvim/lua/plugins/lspconfig.lua

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,37 +1,30 @@
--- lua/plugins/lspconfig.lua
 return {
   {
     "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-      vim.lsp.config("lua_ls", {
+      vim.lsp.config("*", {
         capabilities = capabilities,
       })
-      vim.lsp.enable("lua_ls")
 
-      vim.lsp.config("nixd", {
-        capabilities = capabilities,
-      })
-      vim.lsp.enable("nixd")
-
-      vim.lsp.config("eslint", {
-        capabilities = capabilities,
-      })
-      if vim.fn.executable("vscode-eslint-language-server") == 1 then
-          vim.lsp.enable("eslint")
+      if vim.fn.executable("lua-language-server") == 1 then
+        vim.lsp.enable("lua_ls")
       end
 
-      vim.lsp.config("ts_ls", {
-        capabilities = capabilities,
-      })
+      if vim.fn.executable("nixd") == 1 then
+        vim.lsp.enable("nixd")
+      end
+
+      if vim.fn.executable("vscode-eslint-language-server") == 1 then
+        vim.lsp.enable("eslint")
+      end
+
       if vim.fn.executable("typescript-language-server") == 1 then
         vim.lsp.enable("ts_ls")
       end
 
-      vim.lsp.config("astro", {
-        capabilities = capabilities,
-      })
       if vim.fn.executable("astro-ls") == 1 then
         vim.lsp.enable("astro")
       end
