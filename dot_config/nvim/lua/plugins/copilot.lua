dd519f49a1a930d8936fb767cbc7235b4561cf65
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-11 16:29:01 +0900

    Update .config/nvim/lua/plugins/copilot.lua

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,16 +1,31 @@
+-- return {
+--   "zbirenbaum/copilot.lua",
+--   cmd = "Copilot",
+--   build = ":Copilot auth",
+--   event = "BufReadPost",
+--   config = function()
+--   require("copilot").setup({
+--     suggestion = {
+--       auto_trigger = true,
+--       keymap = {
+--         accept = "<Tab>",
+--       },
+--     },
+--   })
+--   end,
+-- }
+
 return {
   "zbirenbaum/copilot.lua",
-  cmd = "Copilot", -- :Copilotコマンドが実行されたときにプラグインを読み込む
-  build = ":Copilot auth", -- プラグインロード後左記のコマンドを実行
-  event = "BufReadPost", -- ファイルが読み込まれた後にプラグインをロード. https://vim-jp.org/vimdoc-ja/autocmd.html
-  config = function()
-  require("copilot").setup({
+  cmd = "Copilot",
+  event = "BufReadPost",
+  build = ":Copilot auth",
+  opts = {
     suggestion = {
-      auto_trigger = true,
-      keymap = {
-        accept = "<Tab>", -- Tab で提案を受け入れる
-      },
+      enabled = false,
     },
-  })
-  end,
+    panel = {
+      enabled = false,
+    },
+  },
 }
