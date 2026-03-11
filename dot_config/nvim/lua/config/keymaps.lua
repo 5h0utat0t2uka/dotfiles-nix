403cf520e177d004205b16407dad897758d465f5
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-11 18:26:30 +0900

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,15 @@
 -- normal mode keymaps
+-- タブ関連
 vim.keymap.set("n", "gk", "<Cmd>tabnew<CR>", { desc = "New tab" })
 vim.keymap.set("n", "gh", "<Cmd>tabprevious<CR>", { desc = "Previous tab" })
 vim.keymap.set("n", "gl", "<Cmd>tabnext<CR>", { desc = "Next tab" })
 vim.keymap.set("n", "gj", "<Cmd>tabclose<CR>", { desc = "Close tab" })
 vim.keymap.set("n", "gJ", "<Cmd>tabclose!<CR>", { desc = "Force close tab" })
+-- リドゥ
 vim.keymap.set("n", "U", "<C-r>", { noremap = true, silent = true, desc = "Redo" })
+-- 単語の大文字小文字を切り替える
 vim.keymap.set("n", "<leader>tc", "g~iw", { remap = false, desc = "Toggle word case" })
+-- 検索ハイライト解除
 vim.keymap.set("n", "<Esc>", function()
   if vim.v.hlsearch == 1 then
     vim.cmd.nohlsearch()
@@ -17,11 +21,18 @@ vim.keymap.set("n", "<Esc>", function()
     )
   end
 end, { desc = "Clear search highlight or escape" })
--- visual mode keymaps
-vim.keymap.set("x", "<", "<gv", { noremap = true, silent = true })
-vim.keymap.set("x", ">", ">gv", { noremap = true, silent = true })
+
 -- insert mode keymaps
+-- 単語の移動
 vim.keymap.set('i', '<M-w>', '<C-Right>', { desc = "Move words to right" })
 vim.keymap.set('i', '<M-b>', '<C-Left>', { desc = "Move words to left" })
 vim.keymap.set('i', '<M-l>', '<End>', { desc = "Move to end of line" })
 vim.keymap.set('i', '<M-h>', '<Home>', { desc = "Move to sta of line" })
+
+-- visual mode keymaps
+-- インデント
+vim.keymap.set("x", "<", "<gv", { noremap = true, silent = true })
+vim.keymap.set("x", ">", ">gv", { noremap = true, silent = true })
+-- 選択範囲の移動
+vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", { silent = true })
