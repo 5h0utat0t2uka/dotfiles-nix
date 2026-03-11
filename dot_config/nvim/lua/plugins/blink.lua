8414d61c166dec4b7886e622c8ee8950e6258875
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-11 16:29:36 +0900

    Update .config/nvim/lua/plugins/blink.lua

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,9 +1,36 @@
+-- return {
+--   {
+--     "saghen/blink.cmp",
+--     version = "1.*",
+--     dependencies = {
+--       "rafamadriz/friendly-snippets",
+--     },
+--     opts = {
+--       keymap = { preset = "default" },
+--       appearance = {
+--         nerd_font_variant = "mono",
+--       },
+--       completion = {
+--         documentation = { auto_show = false },
+--       },
+--       sources = {
+--         default = { "lsp", "path", "snippets", "buffer" },
+--       },
+--       fuzzy = {
+--         implementation = "prefer_rust_with_warning",
+--       },
+--     },
+--     opts_extend = { "sources.default" },
+--   },
+-- }
+
 return {
   {
     "saghen/blink.cmp",
     version = "1.*",
     dependencies = {
       "rafamadriz/friendly-snippets",
+      "giuxtaposition/blink-cmp-copilot",
     },
     opts = {
       keymap = { preset = "default" },
@@ -14,7 +41,7 @@ return {
         documentation = { auto_show = false },
       },
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "lsp", "path", "snippets", "buffer", "copilot" },
       },
       fuzzy = {
         implementation = "prefer_rust_with_warning",
