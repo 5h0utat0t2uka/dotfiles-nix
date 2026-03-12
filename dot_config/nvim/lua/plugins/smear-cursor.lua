39bb7502e52b37b16257c3e5d83cc554b7c2d2cb
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-13 07:59:55 +0900

    Add .config/nvim/lua/plugins/smear-cursor.lua

---
+++ dot_config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  -- enabled = false,
+  opts = {
+    smear_to_cmd = false,
+    stiffness = 0.5,
+    trailing_stiffness = 0.5,
+    matrix_pixel_threshold = 0.5,
+  },
+}
