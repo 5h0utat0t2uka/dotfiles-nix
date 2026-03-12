64751eb4c81a64d9ad7c70e4b274811138f7c0ed
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-13 08:14:03 +0900

    Update .config/nvim/lua/plugins/blink.lua

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,33 +1,8 @@
--- return {
---   {
---     "saghen/blink.cmp",
---     version = "1.*",
---     dependencies = {
---       "rafamadriz/friendly-snippets",
---     },
---     opts = {
---       keymap = { preset = "default" },
---       appearance = {
---         nerd_font_variant = "mono",
---       },
---       completion = {
---         documentation = { auto_show = false },
---       },
---       sources = {
---         default = { "lsp", "path", "snippets", "buffer" },
---       },
---       fuzzy = {
---         implementation = "prefer_rust_with_warning",
---       },
---     },
---     opts_extend = { "sources.default" },
---   },
--- }
-
 return {
   {
     "saghen/blink.cmp",
     version = "1.*",
+    event = "InsertEnter",
     dependencies = {
       "rafamadriz/friendly-snippets",
       "giuxtaposition/blink-cmp-copilot",
