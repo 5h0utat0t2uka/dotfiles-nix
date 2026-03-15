6287113f6acc46d05dc7b8197f53a11e298efcfc
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-15 15:44:57 +0900

    Update .config/nvim/lua/plugins/noice.lua

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -13,9 +13,9 @@ return {
       cmdline = {
         format = {
           cmdline = { pattern = "^:", icon = "", lang = "vim" },
-          search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
-          search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
-          filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+          search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
+          filter = { pattern = "^:%s*!", icon = "", lang = "bash" },
           lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
           help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
         },
