b92ff0256ff0874daf33eb58a711b1904c959f07
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-15 22:06:47 +0900

    Update .config/nvim/lua/plugins/noice.lua

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -12,12 +12,12 @@ return {
     opts = {
       cmdline = {
         format = {
-          cmdline = { pattern = "^:", icon = "", lang = "vim" },
-          search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
-          search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
-          filter = { pattern = "^:%s*!", icon = "", lang = "bash" },
-          lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
-          help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+          cmdline = {title = "",  pattern = "^:", icon = "", lang = "vim" },
+          search_down = { title = "", kind = "search", pattern = "^/", icon = "", lang = "regex" },
+          search_up = { title = "", kind = "search", pattern = "^%?", icon = "", lang = "regex" },
+          filter = { title = "", pattern = "^:%s*!", icon = "", lang = "bash" },
+          lua = { title = "", pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
+          help = { title = "", pattern = "^:%s*he?l?p?%s+", icon = "" },
         },
       },
       lsp = {
