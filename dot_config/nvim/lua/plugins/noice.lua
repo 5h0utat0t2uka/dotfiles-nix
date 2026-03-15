85f9a6d5c7de0baac2259e27859aad9ce9ab957c
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-15 14:40:17 +0900

    Update .config/nvim/lua/plugins/noice.lua

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -10,6 +10,16 @@ return {
     },
 
     opts = {
+      cmdline = {
+        format = {
+          cmdline = { pattern = "^:", icon = "", lang = "vim" },
+          search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
+          filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+          lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
+          help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+        },
+      },
       lsp = {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
