0dac7445340ac996c7e5c28fc14d8da2c136004c
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-15 21:55:54 +0900

    Update .config/tmux/bin/pane-cwd-label.sh

--- dot_config/tmux/bin/executable_pane-cwd-label.sh
+++ dot_config/tmux/bin/executable_pane-cwd-label.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 
-# p="${1:-}"
-# home="${HOME%/}"
-# p="${p%/}"
-
-# if [[ -z "$p" || "$p" == "$home" ]]; then
-#   printf '~'
-# else
-#   dir_name="${p##*/}"
-#   printf '%s' "${dir_name^^}"
-# fi
 p="${1%/}"
 home="${HOME%/}"
 
 if [[ -z "$p" || "$p" == "$home" ]]; then
   printf '~'
 else
-  # 参照時に最長一致でパスを削り、大文字化する
   dir_name="${p##*/}"
   printf '%s' "${dir_name^^}"
 fi
