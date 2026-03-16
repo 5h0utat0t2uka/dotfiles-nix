59f094749d0ba75657d9e7399b27f7455a0ab3d1
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-17 00:50:12 +0900

    Update .config/nvim/lua/plugins/lspconfig.lua

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -3,31 +3,15 @@ return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
-
       vim.lsp.config("*", {
-        capabilities = capabilities,
+        capabilities = require("blink.cmp").get_lsp_capabilities(),
       })
 
-      if vim.fn.executable("lua-language-server") == 1 then
-        vim.lsp.enable("lua_ls")
-      end
-
-      if vim.fn.executable("nixd") == 1 then
-        vim.lsp.enable("nixd")
-      end
-
-      if vim.fn.executable("vscode-eslint-language-server") == 1 then
-        vim.lsp.enable("eslint")
-      end
-
-      if vim.fn.executable("typescript-language-server") == 1 then
-        vim.lsp.enable("ts_ls")
-      end
-
-      if vim.fn.executable("astro-ls") == 1 then
-        vim.lsp.enable("astro")
-      end
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("nixd")
+      vim.lsp.enable("eslint")
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("astro")
     end,
   },
 }
