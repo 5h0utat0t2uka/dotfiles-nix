94be2e86ab99518b0ec4436232f871d90619733e
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-16 14:12:11 +0900

    remove cliamp from brews

--- nix/modules/darwin/homebrew.nix
+++ nix/modules/darwin/homebrew.nix
@@ -4,7 +4,6 @@
   # Homebrew は GUI アプリ（cask）のみを管理
   homebrew = {
     enable = true;
-
     onActivation = {
       autoUpdate = true;
       upgrade = true;
@@ -12,7 +11,6 @@
     };
 
     taps = [];
-
     brews = [
       # nixpkgs (darwin) で libsmbclient / dyld が壊れているため brew に逃がす
       # https://github.com/nixos/nixpkgs/issues/476308
@@ -22,7 +20,6 @@
       #   args = [ "without-yt-dlp" ];
       # }
     ];
-
     casks = [
       "ghostty"
       "karabiner-elements"
