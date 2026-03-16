fd8ef3a6fc28406c88ebe37cf05bd41f5e434e22
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-17 00:53:01 +0900

    Update .config/nvim/lsp/astro.lua

--- dot_config/nvim/lsp/astro.lua
+++ dot_config/nvim/lsp/astro.lua
@@ -1,7 +1,3 @@
--- return {
---   filetypes = { "astro" },
--- }
-
 return {
   filetypes = { "astro" },
   init_options = {
@@ -9,8 +5,13 @@ return {
   },
   before_init = function(_, config)
     local root = config.root_dir
-    if root then
-      config.init_options.typescript.tsdk = root .. "/node_modules/typescript/lib"
+    if not root then
+      return
+    end
+
+    local tsdk = root .. "/node_modules/typescript/lib"
+    if vim.uv.fs_stat(tsdk) then
+      config.init_options.typescript.tsdk = tsdk
     end
   end,
 }
