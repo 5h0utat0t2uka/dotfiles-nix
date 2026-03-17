3fbfb6997e7571cc4cef0a74449083435c08749b
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-18 08:57:55 +0900

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -24,8 +24,7 @@ return {
       vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#3b4252", bg = "NONE" })
       vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#4c566a", bg = "NONE" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-            -- vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#3b4252", bg = "none" })
-
+       -- vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#3b4252", bg = "none" })
       -- vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#d8dee9", bg = "none" })
       -- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#d8dee9", bg = "none" })
       -- vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#d8dee9", bg = "none" })
@@ -42,10 +41,9 @@ return {
     vim.api.nvim_create_autocmd("InsertEnter", {
       group = group,
       callback = function()
-        vim.api.nvim_set_hl(0, "CursorLine", { bg = "#6C7C6A" })
+        vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2E3440" })
       end,
     })
-
     vim.api.nvim_create_autocmd("InsertLeave", {
       group = group,
       callback = function()
