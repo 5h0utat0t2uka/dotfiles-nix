f61b2e1a56ea6144bce249912e07caa62294f910
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-18 06:52:48 +0900

    Update .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -140,19 +140,19 @@ return {
               -- bg = "#3B4252",
             }
           },
-          {
-            "diff",
-            symbols = {
-              added = ' ',
-              modified = ' ',
-              removed = ' ',
-            },
-            color = {
-              fg = "#5E81AC",
-              bg = "#2E3440",
-            }
-          },
-          { "diagnostics", symbols = { error = ' ', warn = ' ', info = ' ', hint = ' '} }
+          -- {
+          --   "diff",
+          --   symbols = {
+          --     added = ' ',
+          --     modified = ' ',
+          --     removed = ' ',
+          --   },
+          --   color = {
+          --     fg = "#5E81AC",
+          --     bg = "#2E3440",
+          --   }
+          -- },
+          -- { "diagnostics", symbols = { error = ' ', warn = ' ', info = ' ', hint = ' '} }
         },
         -- lualine_c = {},
         lualine_c = {
@@ -170,7 +170,20 @@ return {
               fg = "#5E81AC",
               bg = "#2E3440",
             }
-          }
+          },
+          {
+            "diff",
+            symbols = {
+              added = ' ',
+              modified = ' ',
+              removed = ' ',
+            },
+            color = {
+              fg = "#5E81AC",
+              bg = "#2E3440",
+            }
+          },
+          { "diagnostics", symbols = { error = ' ', warn = ' ', info = ' ', hint = ' '} }
         },
         -- lualine_x = {},
         lualine_x = {
