a19653665921ab1a7c225d60f108bd180826f5e7
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-18 08:18:36 +0900

    Update .config/nvim/lua/plugins/key-menu.lua

--- dot_config/nvim/lua/plugins/key-menu.lua
+++ dot_config/nvim/lua/plugins/key-menu.lua
@@ -4,8 +4,8 @@ return {
     enabled = true,
     config = function()
       vim.o.timeoutlen = 300
-      vim.api.nvim_set_hl(0, "KeyMenuNormal", { fg = "#2E3440", bg = "#4C566A" })
-      vim.api.nvim_set_hl(0, "KeyMenuFloatBorder", { fg = "#4C566A", bg = "#4C566A" })
+      vim.api.nvim_set_hl(0, "KeyMenuNormal", { fg = "#4C566A", bg = "#2E3440" })
+      vim.api.nvim_set_hl(0, "KeyMenuFloatBorder", { fg = "#4C566A", bg = "#2E3440" })
 
       require("key-menu").set("n", "<Space>")
       require("key-menu").set("n", "g")
