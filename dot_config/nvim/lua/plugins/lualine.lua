1e1db871c4619e0f518bebf4ba635d21c08d83ec
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-18 10:07:45 +0900

    Update .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -156,16 +156,16 @@ return {
         },
         -- lualine_c = {},
         lualine_c = {
-          {
-            "filetype",
-            icon_only = true,
-            colored = false,
-            padding = { left = 1, right = 1 },
-            color = {
-              fg = "#5E81AC",
-              bg = "#2E3440",
-            },
-          },
+          -- {
+          --   "filetype",
+          --   icon_only = true,
+          --   colored = false,
+          --   padding = { left = 1, right = 1 },
+          --   color = {
+          --     fg = "#5E81AC",
+          --     bg = "#2E3440",
+          --   },
+          -- },
           {
             "filename",
             file_status = false,
