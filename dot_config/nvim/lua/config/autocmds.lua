f13e4b38460b5a95bf43bc4fbade1d80efeec412
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-30 00:54:47 +0900

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -23,34 +23,34 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end,
 })
 
-vim.api.nvim_create_autocmd("CursorHold", {
-  callback = function()
-    local clients = vim.lsp.get_clients({ bufnr = 0 })
-    if #clients > 0 then
-      local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
-      if #diagnostics > 0 then
-        vim.diagnostic.open_float(nil, {
-          focus = false,
-          scope = "cursor",
-          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-          border = "rounded",
-          source = "if_many",
-        })
-      else
-        vim.lsp.buf.hover()
-      end
-    end
-  end,
-})
-
 -- vim.api.nvim_create_autocmd("CursorHold", {
 --   callback = function()
---     vim.diagnostic.open_float(nil, {
---       focus = false,
---       scope = "cursor",
---       close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
---       border = "rounded",
---       source = "if_many",
---     })
+--     local clients = vim.lsp.get_clients({ bufnr = 0 })
+--     if #clients > 0 then
+--       local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+--       if #diagnostics > 0 then
+--         vim.diagnostic.open_float(nil, {
+--           focus = false,
+--           scope = "cursor",
+--           close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+--           border = "rounded",
+--           source = "if_many",
+--         })
+--       else
+--         vim.lsp.buf.hover()
+--       end
+--     end
 --   end,
 -- })
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, {
+      focus = false,
+      scope = "cursor",
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = "rounded",
+      source = "if_many",
+    })
+  end,
+})
