b385750a98f12214845c21d308ec7236b8f4bc14
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-31 07:15:43 +0900

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -22,6 +22,7 @@ return {
       vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#3b4252", bg = "NONE" })
       vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#4c566a", bg = "NONE" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "MsgArea", { fg = "#88C0D0" })
     end
 
     apply_custom_highlights()
