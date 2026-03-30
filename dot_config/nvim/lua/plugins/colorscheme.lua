c54ebfb14c24459db3d8b0d4945148a6a017a506
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-30 16:20:03 +0900

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   "shaunsingh/nord.nvim",
   lazy = false,
   priority = 1000,
-  init = function()
+  config = function()
     vim.g.nord_contrast = true
     vim.g.nord_borders = true
     vim.g.nord_disable_background = false
@@ -10,11 +10,9 @@ return {
     vim.g.nord_uniform_diff_background = true
     vim.g.nord_bold = true
     vim.cmd.colorscheme("nord")
-  end,
-  config = function()
+
     local function apply_custom_highlights()
       vim.api.nvim_set_hl(0, "CursorLine", { bg = "#353B49" })
-      -- vim.api.nvim_set_hl(0, "Visual", { bg = "#665A6C" })
       vim.api.nvim_set_hl(0, "Visual", { bg = "#544a59" })
       vim.api.nvim_set_hl(0, "TabLine", { fg = "#4C566A", bg = "#2E3440" })
       vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#5E81AC", bg = "#2E3440", bold = true })
@@ -24,30 +22,20 @@ return {
       vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#3b4252", bg = "NONE" })
       vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#4c566a", bg = "NONE" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-       -- vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#3b4252", bg = "none" })
-      -- vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#d8dee9", bg = "none" })
-      -- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#d8dee9", bg = "none" })
-      -- vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#d8dee9", bg = "none" })
-      -- vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#d8dee9", bg = "none" })
     end
 
     apply_custom_highlights()
 
-    local group = vim.api.nvim_create_augroup("MyColorschemeOverrides", { clear = true })
+    local group = vim.api.nvim_create_augroup("NordOverrides", { clear = true })
     vim.api.nvim_create_autocmd("ColorScheme", {
       group = group,
       callback = apply_custom_highlights,
     })
-    vim.api.nvim_create_autocmd("InsertEnter", {
+    vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
       group = group,
-      callback = function()
-        vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2E3440" })
-      end,
-    })
-    vim.api.nvim_create_autocmd("InsertLeave", {
-      group = group,
-      callback = function()
-        vim.api.nvim_set_hl(0, "CursorLine", { bg = "#353B49" })
+      callback = function(e)
+        local bg = e.event == "InsertEnter" and "#2E3440" or "#353B49"
+        vim.api.nvim_set_hl(0, "CursorLine", { bg = bg })
       end,
     })
   end,
