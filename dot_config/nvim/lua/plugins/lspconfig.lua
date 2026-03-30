a929cf9b7d10d259def207c5e71a07e555e7f1da
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-30 14:31:09 +0900

    Update .config/nvim/lua/plugins/lspconfig.lua

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -7,6 +7,24 @@ return {
         capabilities = require("blink.cmp").get_lsp_capabilities(),
       })
 
+      vim.lsp.config("astro", {
+        before_init = function(_, config)
+          if not (config.init_options and config.init_options.typescript) then
+            return
+          end
+          if config.init_options.typescript.tsdk and config.init_options.typescript.tsdk ~= "" then
+            return
+          end
+          local tsc = vim.fn.trim(vim.fn.system("which tsc"))
+          if tsc ~= "" then
+            local nix_tsdk = vim.fn.trim(vim.fn.system("dirname $(dirname " .. tsc .. ")")) .. "/lib/node_modules/typescript/lib"
+            if vim.uv.fs_stat(nix_tsdk) then
+              config.init_options.typescript.tsdk = nix_tsdk
+            end
+          end
+        end,
+      })
+
       vim.lsp.enable("lua_ls")
       vim.lsp.enable("nixd")
       vim.lsp.enable("eslint")
