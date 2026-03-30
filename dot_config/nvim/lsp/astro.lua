6507355c1e25f8514f93adea4f2afc8a51bd210f
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-30 14:29:18 +0900

    Update .config/nvim/lsp/astro.lua

--- dot_config/nvim/lsp/astro.lua
+++ dot_config/nvim/lsp/astro.lua
@@ -14,15 +14,5 @@ return {
         config.init_options.typescript.tsdk = tsdk
         return
     end
-
-    -- fallback: Nixのtscのパスから解決
-    local tsc = vim.fn.trim(vim.fn.system("which tsc"))
-    if tsc and tsc ~= "" then
-      local nix_tsdk = vim.fn.trim(vim.fn.system("dirname $(dirname " .. tsc .. ")")) .. "/lib/node_modules/typescript/lib"
-      if vim.uv.fs_stat(nix_tsdk) then
-        config.init_options.typescript.tsdk = nix_tsdk
-        return
-      end
-    end
   end,
 }
