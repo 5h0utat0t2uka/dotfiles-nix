2e9f6fdf280363e71a150b79160246de32656c4d
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-04-01 08:44:28 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -22,30 +22,25 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('vim._core.ui2').enable({
-  enable = true, -- Whether to enable or disable the UI.
-  msg = { -- Options related to the message module.
-    ---@type 'cmd'|'msg' Default message target, either in the
-    ---cmdline or in a separate ephemeral message window.
-    ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
-    ---or table mapping |ui-messages| kinds and triggers to a target.
-    -- targets = 'cmd',
-    targets = 'cmd',
-    cmd = { -- Options related to messages in the cmdline window.
-      height = 1 -- Maximum height while expanded for messages beyond 'cmdheight'.
-    },
-    dialog = { -- Options related to dialog window.
-      height = 1, -- Maximum height.
-    },
-    msg = { -- Options related to msg window.
-      height = 1, -- Maximum height.
-      timeout = 10000, -- Time a message is visible in the message window.
-    },
-    pager = { -- Options related to message window.
-      height = 1, -- Maximum height.
-    },
-  },
-})
+-- require('vim._core.ui2').enable({
+--   enable = true,
+--   msg = {
+--     targets = 'cmd',
+--     cmd = {
+--       height = 1
+--     },
+--     dialog = {
+--       height = 1,
+--     },
+--     msg = {
+--       height = 1,
+--       timeout = 10000,
+--     },
+--     pager = {
+--       height = 1,
+--     },
+--   },
+-- })
 
 require("lazy").setup({
   spec = {
