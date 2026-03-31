61c0c69f21070e00afde8254b9e391b6565dc87b
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-03-31 19:28:11 +0900

    Update .config/nvim/after/lsp/ts_ls.lua

--- dot_config/nvim/after/lsp/ts_ls.lua
+++ dot_config/nvim/after/lsp/ts_ls.lua
@@ -1,10 +1,16 @@
+-- after/lsp/ts_ls.lua
 return {
-  filetypes = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx",
-  },
+  root_dir = function(bufnr, on_dir)
+    local root = vim.fs.root(bufnr, {
+      "tsconfig.json",
+      "jsconfig.json",
+      "package.json",
+      ".git",
+    })
+    on_dir(root)
+  end,
+
+  reuse_client = function(client, config)
+    return client.name == config.name
+  end,
 }
