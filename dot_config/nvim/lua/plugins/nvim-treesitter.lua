a4bde7029721d52fa8a497c278654b3bec6fe7fe
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-04-04 12:06:39 +0900

    Update .config/nvim/lua/plugins/nvim-treesitter.lua

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,31 +1,62 @@
+-- return {
+--   "nvim-treesitter/nvim-treesitter",
+--   lazy = false,
+--   build = ":TSUpdate",
+--   config = function()
+--     local treesitter = require("nvim-treesitter")
+--     treesitter.setup()
+--     treesitter.install({
+--       "lua", "vim", "vimdoc", "query",
+--       "typescript", "tsx", "json", "javascript", "html", "css", "nix", "astro"
+--     })
+
+--     vim.api.nvim_create_autocmd("FileType", {
+--       pattern = {
+--         "lua", "vim", "typescript", "tsx",
+--         "json", "javascript", "html", "css", "astro"
+--       },
+--       callback = function()
+--         vim.treesitter.start()
+--         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+--       end,
+--     })
+
+--     vim.api.nvim_create_autocmd("FileType", {
+--       pattern = "nix",
+--       callback = function()
+--         vim.treesitter.start()
+--         vim.bo.indentexpr = ""
+--       end,
+--     })
+--   end,
+-- }
 return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   build = ":TSUpdate",
   config = function()
-    local treesitter = require("nvim-treesitter")
-    treesitter.setup()
-    treesitter.install({
+    require("nvim-treesitter").install({
       "lua", "vim", "vimdoc", "query",
-      "typescript", "tsx", "json", "javascript", "html", "css", "nix", "astro"
+      "typescript", "tsx", "json", "javascript",
+      "html", "css", "nix", "astro",
+      "markdown", "markdown_inline",
+      "just", "make", "yaml", "toml",
+      "bash", "zsh", "dockerfile",
+      "gitignore"
     })
 
     vim.api.nvim_create_autocmd("FileType", {
       pattern = {
-        "lua", "vim", "typescript", "tsx",
-        "json", "javascript", "html", "css", "astro"
+        "lua", "vim", "vimdoc", "query",
+        "typescript", "tsx", "json", "javascript",
+        "html", "css", "nix", "astro",
+        "markdown",
+        "just", "make", "yaml", "toml",
+        "bash", "zsh", "dockerfile",
+        "gitignore",
       },
-      callback = function()
-        vim.treesitter.start()
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "nix",
-      callback = function()
-        vim.treesitter.start()
-        vim.bo.indentexpr = ""
+      callback = function(args)
+        vim.treesitter.start(args.buf)
       end,
     })
   end,
