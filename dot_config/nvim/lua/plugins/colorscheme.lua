82d15b12798886a927787912d019e07853d8dbac
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-04-05 14:49:01 +0900

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -13,6 +13,12 @@ return {
 
     local function apply_custom_highlights()
       vim.api.nvim_set_hl(0, "CursorLine", { bg = "#353B49" })
+
+      vim.api.nvim_set_hl(0, "nCursor", { fg = "#2E3440", bg = "#88C0D0" })
+      vim.api.nvim_set_hl(0, "iCursor", { fg = "#2E3440", bg = "#A3BE8C" })
+      vim.api.nvim_set_hl(0, "rCursor", { fg = "#ECEFF4", bg = "#BF616A" })
+      vim.api.nvim_set_hl(0, "oCursor", { fg = "#2E3440", bg = "#EBCB8B" })
+
       vim.api.nvim_set_hl(0, "Visual", { bg = "#544a59" })
       vim.api.nvim_set_hl(0, "TabLine", { fg = "#4C566A", bg = "#2E3440" })
       vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#81A1C1", bg = "#2E3440", bold = true })
