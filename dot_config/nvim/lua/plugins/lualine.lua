84d8c24a96309fbd4313757f23997f013f1870c4
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-04-11 04:30:14 +0900

    Update .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -181,7 +181,7 @@ return {
             -- padding = { left = 1, right = 1 },
             -- separator = { left = " ", right = " " },
             color = {
-              fg = "#81A1C1",
+              fg = "#4C566A",
               bg = "#2E3440",
               -- fg = "#2E3440",
               -- bg = "#4C566A",
