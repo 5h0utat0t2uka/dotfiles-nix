14561b3910c53eebc7798c494a0360fa5978ce95
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-04-12 20:30:24 +0900

    Update .config/nvim/lua/plugins/undo-glow.lua

--- dot_config/nvim/lua/plugins/undo-glow.lua
+++ dot_config/nvim/lua/plugins/undo-glow.lua
@@ -14,7 +14,7 @@ return {
       yank  = { hl_color = { bg = "#6e596d" } },
       paste = { hl_color = { bg = "#61754e" } },
       comment = { hl_color = { bg = "#4C566A" } },
-      cursor  = { hl_color = { bg = "#8a5c4a" } },
+      search  = { hl_color = { bg = "#8a5c4a" } },
     },
   },
   keys = {
