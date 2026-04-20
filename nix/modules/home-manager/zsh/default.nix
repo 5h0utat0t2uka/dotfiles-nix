546bf2fc312ed1efe25e18198b573f28c2d67260
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-04-20 20:57:18 +0900

    restore $HOME/.local/bin in PATH

--- nix/modules/home-manager/zsh/default.nix
+++ nix/modules/home-manager/zsh/default.nix
@@ -1,9 +1,8 @@
 { ... }:
 
 {
-  # XDG base directories を Home Manager 管理
   xdg.enable = true;
-  # ~/.zshenv は zsh が最初に読むので ZDOTDIR はここで設定する
+
   home.file.".zshenv".text = ''
     # XDG Base Directory Specification
     export XDG_CONFIG_HOME="''${XDG_CONFIG_HOME:-$HOME/.config}"
@@ -16,14 +15,11 @@
     # nb
     export NBRC_PATH="$XDG_CONFIG_HOME/nb/.nbrc"
   '';
-  # ~/.config/zsh 配下
+
   xdg.configFile = {
     "zsh/.zprofile".source = ./zprofile;
     "zsh/.zshrc".source = ./zshrc;
     "zsh/.p10k.zsh".source = ./p10k.zsh;
   };
-  # $HOME/.local/bin を PATH に追加
-  home.sessionPath = [
-    "$HOME/.local/bin"
-  ];
+  # home.sessionPath は nix-darwin の /etc/zshrc 上書きの影響で zsh に反映されないため .zprofile で直接設定する
 }
