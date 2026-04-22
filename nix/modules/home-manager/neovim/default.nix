56e796c2ba47a0b39e5c2a5ec53305288135364d
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-04-22 17:17:34 +0900

    update

--- nix/modules/home-manager/neovim/default.nix
+++ nix/modules/home-manager/neovim/default.nix
@@ -19,11 +19,10 @@ in
   xdg.configFile = {
     # Nix store の mtime epoch 0 により vim.loader のバイトコードキャッシュが古いままロードされ続ける問題を回避するため、Nix store を経由しない
     # 直接リンク（mkOutOfStoreSymlink）で配置するので、設定変更は darwin-rebuild switch なしで即反映されるようになる
+    "nvim/lazy-lock.json".source = config.lib.file.mkOutOfStoreSymlink "${configRoot}/lazy-lock.json";
+    "nvim/.luarc.json".source = config.lib.file.mkOutOfStoreSymlink "${configRoot}/.luarc.json";
     "nvim/lua".source = config.lib.file.mkOutOfStoreSymlink "${configRoot}/lua";
     "nvim/after".source = config.lib.file.mkOutOfStoreSymlink "${configRoot}/after";
     "nvim/snippets".source = config.lib.file.mkOutOfStoreSymlink "${configRoot}/snippets";
-    "nvim/.luarc.json".source = config.lib.file.mkOutOfStoreSymlink "${configRoot}/.luarc.json";
-
-    "nvim/lazy-lock.json".source = config.lib.file.mkOutOfStoreSymlink "${configRoot}/lazy-lock.json";
   };
 }
