54638687cc8d4a3a4ff02ad4e80365455b0db41d
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-04-25 18:06:24 +0900

    update p10k

--- nix/modules/home-manager/zsh/p10k.zsh
+++ nix/modules/home-manager/zsh/p10k.zsh
@@ -591,7 +591,7 @@
 
   #######################[ direnv: direnv status (https://direnv.net/) ]########################
   # Direnv color.
-  typeset -g POWERLEVEL9K_DIRENV_FOREGROUND=5
+  typeset -g POWERLEVEL9K_DIRENV_FOREGROUND=4
   # Custom icon.
   typeset -g POWERLEVEL9K_DIRENV_VISUAL_IDENTIFIER_EXPANSION=' direnv'
 
