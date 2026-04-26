52f85ec9e5b404ebdc553f91e0b4dbf9a6dc161e
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-04-26 11:51:31 +0900

    override direnv doCheck for build failure issue

--- nix/modules/home-manager/direnv/default.nix
+++ nix/modules/home-manager/direnv/default.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.direnv = {
     enable = true;
+    package = pkgs.direnv.overrideAttrs (old: {
+      # Darwin でチェックをスキップ
+      # https://github.com/NixOS/nixpkgs/issues/507531?utm_source=chatgpt.com
+      doCheck = (old.doCheck or true) && !pkgs.stdenv.isDarwin;
+    });
     nix-direnv.enable = true;
   };
 }
