587d02c2c6aba67d8420a1b15c6c1a8958f51eb8
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-05-03 18:53:40 +0900

    update starship config

--- nix/modules/home-manager/starship/default.nix
+++ nix/modules/home-manager/starship/default.nix
@@ -7,12 +7,18 @@
       scan_timeout = 25;
       command_timeout = 500;
       add_newline = true;
-
       format = ''
-        [┌─](bold bright-black) $hostname $directory $git_branch$git_status$fill$cmd_duration$nodejs''${custom.nix_shell}$time
+        [┌─](bold bright-black) $hostname $directory $git_branch$git_status$fill$cmd_duration$nodejs$nix_shell$time
         [└─](bold bright-black) $character
       '';
 
+      character = {
+        success_symbol = "[❯](green)";
+        error_symbol = "[❯](red)";
+      };
+      fill = {
+        symbol = " ";
+      };
       hostname = {
         ssh_only = false;
         format = "[$hostname](green)";
@@ -33,38 +39,38 @@
         conflicted = "[ =\${count}](bold red)";
         ahead = "[ \${count}](blue)";
         behind = "[ \${count}](blue)";
-        diverged = "[ ⇕⇡\${ahead_count}\${behind_count}](purple)";
+        diverged = "[ \${ahead_count}\${behind_count}](purple)";
         untracked = "[ ?\${count}](blue)";
         stashed = "[ *\${count}](purple)";
         modified = "[ !\${count}](yellow)";
         staged = "[ +\${count}](green)";
-        renamed = "[ ~\${count}](purple)";
+        renamed = "[ ~\${count}](yellow)";
         deleted = "[ -\${count}](red)";
       };
       cmd_duration = {
         min_time = 3000;
         format = "[$duration](yellow)  ";
       };
-      nodejs = {
-        symbol = " ";
+      bun = {
+        symbol = " ";
+        version_format = "$raw";
         format = "[$symbol$version](green)  ";
       };
-      custom.nix_shell = {
-        when = ''test -n "$IN_NIX_SHELL"'';
-        command = ''printf " %s" "$IN_NIX_SHELL"'';
-        format = "[$output](blue)  ";
+      nodejs = {
+        symbol = " ";
+        version_format = "$raw";
+        format = "[$symbol$version](green)  ";
+      };
+      nix_shell = {
+        symbol = " ";
+        format = "[$symbol$state](blue)  ";
+        impure_msg = "impure";
+        pure_msg = "pure";
       };
       time = {
         disabled = false;
         format = "[$time](bright-black)";
         time_format = "%H:%M:%S";
-      };
-      character = {
-        success_symbol = "[❯](bold green)";
-        error_symbol = "[❯](bold red)";
-      };
-      fill = {
-        symbol = " ";
       };
     };
   };
